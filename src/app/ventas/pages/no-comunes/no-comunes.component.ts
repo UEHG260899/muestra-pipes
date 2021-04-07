d543919cb0d234f57324b4c08d58b450c094b79b
Uriel <urielenrique99@gmail.com>
2021-04-07 13:58:14 -0500

    Se termino el apartado de pipes de angular

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { interval } from "rxjs";
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +7,75 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  controlNombre: boolean = true;
 
-  ngOnInit(): void {
+  //i18nSelect
+  nombre: string = 'Uriel';
+  genero: string = 'masculino';
+
+  invitacionMap = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
   }
 
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Cris', 'Carlos', 'Lorena'];
+
+  clientesMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente experando',
+    'other': 'tenemos # clientes esperando' //el numeral indica el numero de elementos del arreglo
+  }
+
+
+  cambiarPersona() {
+    this.controlNombre = !this.controlNombre;
+
+    if (this.controlNombre) {
+      this.nombre = 'Uriel';
+      this.genero = 'masculino';
+      return;
+    }
+    this.nombre = 'Maria';
+    this.genero = 'femenino';
+  }
+
+  borrarCiente(){
+    this.clientes.pop();
+  }
+
+  //KeyValue Pipe
+
+  persona = {
+    nombre: 'Enrique',
+    edad: 21,
+    direccion: 'Mexico'
+  }
+
+  //JSON Pipe
+
+  heroes: Object[] = [
+    {
+      nombre : 'Superman',
+      vuela : true
+    },
+    {
+      nombre : 'Batman',
+      vuela : false
+    },
+    {
+      nombre : 'Robin',
+      vuela : false
+    }
+  ];
+
+  //Async pipe
+  miObservable = interval(1000);
+  miPromesa = new Promise( (resolve, reject) =>{
+    setTimeout(() => {
+      resolve('Tenemos datos de promesa')
+    }, 3500);
+  } )
 }
