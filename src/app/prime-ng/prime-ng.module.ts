df758c55f22a8cabe50911adbf361204b6dee821
Uriel <urielenrique99@gmail.com>
2021-04-06 19:19:43 -0500

    Se agreg la estructura basica de los ficheros

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 //PrimeNG
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from "primeng/card";
+import { MenubarModule } from "primeng/menubar";
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ButtonModule,
-    CardModule
-  ],
-
   exports: [
     ButtonModule,
-    CardModule
+    CardModule,
+    MenubarModule
   ]
 })
 export class PrimeNgModule { }
