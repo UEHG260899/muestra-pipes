f0cc0616ddc644c271ae88db2be0c80120489f09
Uriel <urielenrique99@gmail.com>
2021-04-07 23:33:51 -0500

    Se agrego funcionalidad a la seccion de pipes personalizados

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -5,6 +5,9 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from "primeng/card";
 import { MenubarModule } from "primeng/menubar";
 import { FieldsetModule } from 'primeng/fieldset';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+
 
 
 
@@ -13,7 +16,9 @@ import { FieldsetModule } from 'primeng/fieldset';
     ButtonModule,
     CardModule,
     MenubarModule,
-    FieldsetModule
+    FieldsetModule,
+    TableModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,11 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent  {
+  enMayusculas: boolean = true;
+  ordenarPor: string = '';
+  heroes: Heroe[] = [
+    {
+      nombre : 'SuperMan',
+      vuela : true,
+      color : Color.azul
+    },
+    {
+      nombre : 'Batman',
+      vuela : false,
+      color : Color.negro
+    },
+    {
+      nombre : 'Robin',
+      vuela : false,
+      color : Color.verde
+    },
+    {
+      nombre : 'Daredevil',
+      vuela : false,
+      color : Color.rojo
+    },
+    {
+      nombre : 'Green Lantern',
+      vuela : true,
+      color : Color.verde
+    },
+  ]
 
-  constructor() { }
-
-  ngOnInit(): void {
+  cambiarTipado(){
+    this.enMayusculas = !this.enMayusculas;
   }
 
+  cambiarOrden(valor: string){
+    this.ordenarPor = valor;
+  }
 }
